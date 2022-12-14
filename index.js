c70f6469dc94c22ab5064b7b42784f092ea0471d
Alott01 <Alott01@users.noreply.github.com>
2022-12-14 11:09:49 +0100

    final proyect

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
+/*====================================VARIABLES==================================*/
 let options =['rock', 'paper', 'scicer', 'alligator','Spock'];
-let diccionary=
-{
+let diccionary={
     'rock':{
         'rock':'tie', 'paper':'loser', 'scicer':'winner', 'alligator':'winner', 'Spock':'loser'
     },
@@ -17,62 +17,174 @@ let diccionary=
         'rock':'winner', 'paper':'loser', 'scicer':'winner', 'alligator':'loser', 'Spock':'tie'
     }
 }
-
+let scoreUser=0;
+let iaScore=0;
+let scoreUser1=0;
+let scoreUser2=0;
+let scoreMachine=0;
+let control=0;
+let optionUser=undefined;
+let optionUser1=undefined;
+let optionUser2=undefined;
+let resultText=undefined;
+/*================================GENERAL FUNTIONS===============================*/
+/*************************CHANGE***************************/
+function hiddeResult(){
+    let elements = document.getElementsByClassName("results");
+    elements[elements.length-1].style.display = "none";
+}
+function showResult(){
+    let elements = document.getElementsByClassName("results");
+    elements[elements.length-1].style.display="";
+}
+/*************************RANDOME***************************/
 function random(min, max) {
     return Math.floor((Math.random() * (max - min + 1)) + min);
 }
-
-function userVSuser(user1, user2){
-    let eleccionUsuario1=options[user1];
-    let eleccionUsuario2=options[user2];
-    console.log('> Tu: '+eleccionUsuario1+' vs Usuario2: '+eleccionUsuario2);
-    switch(diccionary[eleccionUsuario][eleccionMaquina]){
-        case 'winner':
-            console.log('> you won');
-            break;
-        case 'loser':
-            console.log('> you lose');
-            break;
-        case 'tie':
-            console.log('> you have tied');
-            break;
-    }
+/*==============================FUNTIONS FOR THE GAME============================*/
+/**********************VS MACHINE***************************/
+function myFunction1(){
+    let optionUser='rock'
+    userVSmachine(optionUser);
 }
-
-
-function userVSmachine(Usuario) {
+function myFunction2(){
+    let optionUser='Spock'
+    userVSmachine(optionUser);
+}
+function myFunction3(){
+    let optionUser='paper'
+    userVSmachine(optionUser);
+}
+function myFunction4(){
+    let optionUser='scicer'
+    userVSmachine(optionUser);
+}
+function myFunction5(){
+    let optionUser='alligator'
+    userVSmachine(optionUser);
+}
+function userVSmachine(usuario) {
     let eleccionMaquina=options[random(0,4)];
-    let eleccionUsuario =options[Usuario];
-    console.log('> Tu: '+eleccionUsuario+' vs Maquina: '+eleccionMaquina);
-    switch(diccionary[eleccionUsuario][eleccionMaquina]){
+    switch(diccionary[usuario][eleccionMaquina]){
         case 'winner':
-            console.log('> you won');
+            scoreUser++;
+            /*****/
+            resultText=document.getElementsByClassName("results_text");
+            resultText[resultText.length-1].innerHTML = `<p>USER: ${usuario} && IA: ${eleccionMaquina}</p>
+            <br><p>THE WINNER IS PLAYER 1</p>`;
+            document.getElementById("playerScore1").innerHTML =`PLAYER 1 SCORE: ${scoreUser}`;
+            document.getElementById("iaScore").innerHTML =`IA SCORE: ${iaScore}`;
+            showResult();
             break;
         case 'loser':
-            console.log('> you lose');
+            iaScore++;
+            resultText=document.getElementsByClassName("results_text");
+            resultText[resultText.length-1].innerHTML = `<p>USER: ${usuario} && IA: ${eleccionMaquina}</p>
+            <br><p>THE WINNER IS THE IA</p>`;
+            document.getElementById("playerScore1").innerHTML =`PLAYER 1 SCORE: ${scoreUser}`;
+            document.getElementById("iaScore").innerHTML =`IA SCORE: ${iaScore}`;
+            showResult();
             break;
         case 'tie':
-            console.log('> you have tied');
+            resultText=document.getElementsByClassName("results_text");
+            resultText[resultText.length-1].innerHTML = `<p>USER: ${usuario} && IA: ${eleccionMaquina}</p>
+            <br><p>YOU HAVE TIE</p>`;
+            document.getElementById("playerScore1").innerHTML =`PLAYER 1 SCORE: ${scoreUser}`;
+            document.getElementById("iaScore").innerHTML =`IA SCORE: ${iaScore}`;
+            showResult();
             break;
     }
 }
-
-function contador(){
-
-}
-
-/**
- * function gameMode(x){
-    if(x==1){
-        usuario=prompt('> you have selected man vs machine mode choose: ','');
-        userVSmachine(usuario);
-    }else if(x==2){
-        usuario1=prompt('> you have selected man vs machine mode choose: ','');
-        usuario2=prompt('choose: ', '');
-        userVSuser(usuario1, usuario2);
+/**********************VS PLAYER****************************/
+function myFunction6(){
+    if(control==0){
+        optionUser1='rock';
+        document.getElementById("player").innerHTML='PLAYER 2: ';
+        control++;
     }else{
-        prompt('try again');
+        optionUser2='rock';
+        document.getElementById("player").innerHTML='PLAYER 1: ';
+        control=0;
+        userVSuser(optionUser1, optionUser2);
     }
 }
- */
-
+function myFunction7(){
+    if(control==0){
+        optionUser1='Spock';
+        document.getElementById("player").innerHTML='PLAYER 2: ';
+        control++;
+    }else{
+        optionUser2='Spock';
+        document.getElementById("player").innerHTML='PLAYER 1: ';
+        control=0;
+        userVSuser(optionUser1, optionUser2);
+    }
+}
+function myFunction8(){
+    if(control==0){
+        optionUser1='paper';
+        document.getElementById("player").innerHTML='PLAYER 2: ';
+        control++;
+    }else{
+        optionUser2='paper';
+        document.getElementById("player").innerHTML='PLAYER 1: ';
+        control=0;
+        userVSuser(optionUser1, optionUser2);
+    }
+}
+function myFunction9(){
+    if(control==0){
+        optionUser1='scicer';
+        document.getElementById("player").innerHTML='PLAYER 2: ';
+        control++;
+    }else{
+        optionUser2='scicer';
+        document.getElementById("player").innerHTML='PLAYER 1: ';
+        control=0;
+        userVSuser(optionUser1, optionUser2);
+    }
+}
+function myFunction10(){
+    if(control==0){
+        optionUser1='alligator';
+        document.getElementById("player").innerHTML='PLAYER 2: ';
+        control++;
+    }else{
+        optionUser2='alligator';
+        document.getElementById("player").innerHTML='PLAYER 1: ';
+        control=0;
+        userVSuser(optionUser1, optionUser2);
+    }
+}
+function userVSuser(user1, user2){
+    switch(diccionary[user1][user2]){
+        case 'winner':
+            scoreUser1++;
+            resultText=document.getElementsByClassName("results_text");
+            resultText[resultText.length-1].innerHTML = `<p>USER: ${user1} && IA: ${user2}</p>
+            <br><p>THE WINNER IS PLAYER 1</p>`;
+            document.getElementById("playerScore1").innerHTML =`PLAYER 1 SCORE: ${scoreUser1}`;
+            document.getElementById("playerScore2").innerHTML =`PLAYER 2 SCORE: ${scoreUser2}`;
+            showResult();
+            break;
+        case 'loser':
+            scoreUser2++;
+            resultText=document.getElementsByClassName("results_text");
+            resultText[resultText.length-1].innerHTML = `<p>USER: ${user1} && IA: ${user2}</p>
+            <br><p>THE WINNER IS PLAYER 2</p>`;
+            document.getElementById("playerScore1").innerHTML =`PLAYER 1 SCORE: ${scoreUser1}`;
+            document.getElementById("playerScore2").innerHTML =`PLAYER 2 SCORE: ${scoreUser2}`;
+            showResult();
+            break;
+        case 'tie':
+            resultText=document.getElementsByClassName("results_text");
+            resultText[resultText.length-1].innerHTML = `<p>USER: ${user1} && IA: ${user2}</p>
+            <br><p>YOU HAVE TIE</p>`;
+            document.getElementById("playerScore1").innerHTML =`PLAYER 1 SCORE: ${scoreUser1}`;
+            document.getElementById("playerScore2").innerHTML =`PLAYER 2 SCORE: ${scoreUser2}`;
+            showResult();
+            break;
+    }
+}
+/*============================================================================================*/
+hiddeResult();
